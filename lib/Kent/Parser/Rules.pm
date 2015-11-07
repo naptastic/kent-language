f54f48b2235385745f62a8c3452a93cf7c3a4226
David Nielson <naptastic@gmail.com>
2015-11-06 20:08:32 -0600

    put sample code back where it was
    move the experiments to a file that won't grunk up my testing

--- lib/Kent/Parser/Rules.pm
+++ lib/Kent/Parser/Rules.pm
@@ -4,13 +4,28 @@ use common::sense;
 
 our @rules = (
 
-    [ qr/ (DIGITS)? (DOT) (DIGITS) /x, 'Number', ],
+    # This file is just a scratchpad for ideas right now, because I really
+    # don't know what I'm doing.
+
+    [ qr/ (DIGITS)? (DOT)? (DIGITS) /x, 'Number', ],
     [ qr/ Number OP_ADD Number /x,     'Addition', ],
+
+    # A block is an array of zero or more statements.
+    # A statement is a structure made of expressions.
+    # Expressions have many possible forms.
+
+    # Conditionals
 
     # statement?
     # expression?
     # math op?
-    # Ternary op - that ought to be fun
+
+    # TERNARY    := COND ? STMT : STMT
+    # ASSIGN     := FQID '=' EXPR
+    # FQID       := ID (DOT ID)*
+    # TERM       := ID | NUMBER | STRING
+    # ARITH      := TERM (arithmetic operator) TERM
+
     [ qr/ (ID)? (DOT ID) /x, 'Access' ],
 
 );
