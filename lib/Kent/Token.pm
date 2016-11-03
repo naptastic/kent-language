3e0c9c7894961dbc38b492ff9be14c98eca2e394
David Nielson <david@naptastic.com>
2016-11-03 07:02:48 -0500

    Reorganize keywords more logically

--- lib/Kent/Token.pm
+++ lib/Kent/Token.pm
@@ -5,20 +5,21 @@ use Kent::Lexer::Rules    ();
 use Kent::Lexer::Keywords ();
 
 my $rule_table = Kent::Lexer::Rules::table;
-my %keywords   = %Kent::Lexer::Keywords::keywords;
+my @keywords   = @Kent::Lexer::Keywords::keywords;
 
 sub new {
     my ( $class, %args ) = @_;
 
-    my $self = { 'name'         => $args{name},
-                 'raw'          => $args{raw},
-                 'width'        => length( $args{raw} ), #XXX: This is currently wrong for opening and closing quotes.
-                 'line'         => $args{line},
-                 'column'       => $args{column},
-                 'next_context' => $args{next_context}};
+    my $self = {
+              'name'         => $args{name},
+              'raw'          => $args{raw},
+              'width'        => length( $args{raw} ),    #XXX: This is currently wrong for opening and closing quotes.
+              'line'         => $args{line},
+              'column'       => $args{column},
+              'next_context' => $args{next_context} };
 
     # Is this a keyword?
-    if ( $self->{name} eq 'ID' && exists $keywords{ $self->{raw} } ) {
+    if ( $self->{name} eq 'ID' && grep { $_ eq $self->{raw} } @keywords ) {
         $self->{name} = "kw_$self->{raw}";
     }
 
